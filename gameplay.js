b8846f9be3497463f0a09c29b6c1d739e1a84960
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-28 14:51:24 -0700

    Things drop and dolphin folows

--- gameplay.js
+++ gameplay.js
@@ -3,16 +3,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
   game.load.image('water', 'assets/sky.png');
   game.load.image('dolphin', 'assets/diamond.png');
+  game.load.image('ball', 'assets/pangball.png');
 
 }
 
 var player;
 var swim;
+var words;
+var score = 0;
+var total = 0;
 
 function create() {
   //background water created/position
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.gravity.y = 150;
   game.add.sprite(0, 0, 'water');
+
 
   //player created/position
   player = game.add.sprite(game.world.centerX, game.world.centerY, 'dolphin');
@@ -21,17 +27,40 @@ function create() {
   player.body.gravity.y = 300;
   player.body.collideWorldBounds = true;
 
+  //Addint shift as swim key
   swim = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+
+  //Scoreboard
+  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'});
+
+  words = game.add.group();
+  stars.enableBody = true;
+
+  //Create initial function instance
+  createBall()
+
+}
+
+function createBall() {
+
+  var word = words.create(game.world.randomX, (Math.random() * 50), 'ball');
+  // game.physics.enable(word, Phaser.Physics.ARCADE);
+  ball.body.bounce.y = 0.9;
+  ball.body.collideWorldBounds = true;
+
+  total ++;
 
 }
 
 function update() {
 
+    game.physics.arcade.overlap(player, words, collectWord, null, this);
+
   //  only move when you click
   if (swim.isDown)
   {
       //  400 is the speed it will move towards the mouse
-      game.physics.arcade.moveToPointer(player, 400);
+      game.physics.arcade.moveToPointer(player, 350);
 
       //  if it's overlapping the mouse, don't move any more
       if (Phaser.Rectangle.contains(player.body, game.input.x, game.input.y))
@@ -39,9 +68,18 @@ function update() {
           player.body.velocity.setTo(0, 0);
       }
   }
-  else
-  {
+  else{
       player.body.velocity.setTo(0, 0);
   }
+
+  if (total < 10){
+      createBall();
+  }
+
+  function collectWord (player, words) {
+  words.kill();
+  score += 10;
+  scoreText.text = 'Score: ' + score;
+}
 
 }
