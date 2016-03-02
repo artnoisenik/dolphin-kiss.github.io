18f984ebee58f2ad05c9b3f6897a992982f7f826
Phil Skaggs <prskaggsjr@gmail.com>
2016-03-01 22:12:42 -0700

    Health bar takes hits/lives loss/no game over or reset yet

--- js/gameplay.js
+++ js/gameplay.js
@@ -57,20 +57,27 @@ function preload() {
   game.load.image('dolphin', 'assets/diamond.png');
   game.load.image('ball', 'assets/pangball.png');
   game.load.image('bullet', 'assets/bullet.png');
+  game.load.image('enemyBullet', 'assets/enemy-bullet.png');
+
 
 }
 
 var player;
+var lives = 3;
+
 var bullets;
 var fireRate = 300;
 var nextFire = 0;
+
+var enemyBullet;
+var firingTimer = 0;
+var livingEnemies = [];
 
 var swim;
 var words;
 var highScore = 0;
 var score = 0;
 var total = 0;
-var text1;
 
 
 function create() {
@@ -89,31 +96,53 @@ function create() {
   player.body.gravity.y = 300;
   player.body.collideWorldBounds = true;
 
-  //Addint shift as swim key
+//Addint shift as swim key
   swim = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
 
-  //Scoreboard
+//Scoreboard
   scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#fff'});
 
   words = game.add.group();
   words.enableBody = true;
 
-  //Heathbar
+//Heathbar
+  game.add.text(500, 16, 'Health', { font: '25px Arial', fill: '#fff' });
   var barConfig = {x: 200, y: 100, width: 200, height: 20};
   this.myHealthBar = new HealthBar(this.game, barConfig);
-  this.myHealthBar.setPosition(600, 36);
+  this.myHealthBar.setPosition(600, 56);
+  this.healthValue = 100;
 
-  // Create initial function instance
+
+// Create initial function instance
   createBall()
 
-  //Player Bullets
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+//Player Bullets
+  bullets = game.add.group();
+  bullets.enableBody = true;
+  bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
-    bullets.createMultiple(50, 'bullet');
-    bullets.setAll('checkWorldBounds', true);
-    bullets.setAll('outOfBoundsKill', true);
+  bullets.createMultiple(50, 'bullet');
+  bullets.setAll('checkWorldBounds', true);
+  bullets.setAll('outOfBoundsKill', true);
+
+// The enemy's bullets
+  enemyBullets = game.add.group();
+  enemyBullets.enableBody = true;
+  enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+  enemyBullets.createMultiple(30, 'enemyBullet');
+  enemyBullets.setAll('anchor.x', 0.5);
+  enemyBullets.setAll('anchor.y', 1);
+  enemyBullets.setAll('outOfBoundsKill', true);
+  enemyBullets.setAll('checkWorldBounds', true);
+
+//Player lives
+    livesText = game.add.text(16, 56, 'lives : 3', { font: '25px Arial', fill: '#fff' });
+
+//Start text
+introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+introText.anchor.setTo(0.5, 0.5);
+
+game.input.onDown.add(createBall, this);
 
 
 }
@@ -143,10 +172,67 @@ function fire() {
 
 }
 
+function enemyFires () {
+
+    //  Grab the first bullet we can from the pool
+    enemyBullet = enemyBullets.getFirstExists(false);
+
+    livingEnemies.length=0;
+
+    words.forEachAlive(function(alien){
+
+        // put every living enemy in an array
+        livingEnemies.push(word);
+    });
+
+    if (enemyBullet && livingEnemies.length > 0)
+    {
+
+        var random=game.rnd.integerInRange(0,livingEnemies.length-1);
+
+        // randomly select one of them
+        var shooter=livingEnemies[random];
+        // And fire the bullet from this enemy
+        enemyBullet.reset(shooter.body.x, shooter.body.y);
+
+        game.physics.arcade.moveToObject(enemyBullet,player,320);
+        firingTimer = game.time.now + 2000;
+    }
+
+}
+
+function enemyHitsPlayer (player,bullet) {
+
+    bullet.kill();
+
+    this.healthValue = this.healthValue - 10;
+    // if(this.healthValue === 0) this.healthValue = 0;
+    this.myHealthBar.setPercent(this.healthValue);
+
+    // this.healthValue = player.health - 10;
+
+    console.log(lives);
+
+    if (this.healthValue === 0)
+    {
+      lives--;
+      livesText.text = 'lives: ' + lives;
+      if (lives === 0)
+   {
+       gameOver();
+   }
+   else{
+      this.healthValue = 110;
+
+    }
+    }
+
+}
+
 function update() {
 
     game.physics.arcade.overlap(bullets, words, collectWord, null, this);
-    // game.physics.arcade.overlap(player, text1, collectWord, null, this);
+    game.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
 
   //  only move when you click
   if (swim.isDown)
@@ -171,6 +257,12 @@ function update() {
     fire();
   }
 
+  //fire enemy bullet
+  if (game.time.now > firingTimer)
+{
+    enemyFires();
+}
+
   //create enemies
   if (total < 10){
       createBall();
@@ -181,7 +273,7 @@ function update() {
   word.kill();
   total--;
   score += 10;
-  scoreText.text = 'Score: ' + score;
+  scoreText.text = 'score: ' + score;
     if(score > highScore){
       highScore = score;
       console.log(highScore);
@@ -189,6 +281,15 @@ function update() {
   }
 
   }
+
+}
+
+function gameOver () {
+
+    player.velocity.setTo(0, 0);
+
+    introText.text = 'Game Over!';
+    introText.visible = true;
 
 }
 
